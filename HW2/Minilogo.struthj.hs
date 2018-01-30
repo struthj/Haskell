ea26d4dfe0cdc883d05fe58ef2a90a5bcd3f5bd9
struthj <struthj@oregonstate.edu>
2018-01-30 01:39:23 -0800

    Finished HW2 Minilogo

---
+++ HW2/Minilogo.struthj.hs
@@ -0,0 +1,117 @@
+module Minilogo where
+
+
+import Prelude hiding (Num)
+import Data.List
+
+--
+-- * Syntax of Minilogo
+--
+
+-- num	::=	(any natural number)	
+-- var	::=	(any variable name)	
+-- macro	::=	(any macro name)	
+ 			
+-- prog	::=	ε   |   cmd ; prog	sequence of commands
+ 			
+-- mode	::=	down   |   up	pen status
+ 			
+-- expr	::=	var	variable reference
+-- |	num	literal number
+-- |	expr + expr	addition expression
+ 			
+-- cmd	::=	pen mode	change pen mode
+-- |	move ( expr , expr )	move pen to a new position
+-- |	define macro ( var* ) { prog }  	define a macro
+-- |	call macro ( expr* )	invoke a macro
+
+-- 1. Encode the above grammar as a set of Haskell data types
+
+type Num = Int
+type Var = String
+
+data Mode = Up
+          | Down
+  deriving(Eq,Show)
+
+data Expr = V Var
+          | N Num
+          | Add Expr Expr
+  deriving(Eq,Show)
+
+
+type Macro = String
+
+type Prog = [Cmd]
+
+data Cmd = Pen Mode
+         | Move Expr Expr
+         | Define Macro [Var] Prog
+         | Call Macro [Expr]
+  deriving (Eq,Show)
+
+
+-- line (x1,y1,x2,y2)
+-- define line (x1,y1,x2,y2) {
+--   pen up; move (x1,y1);
+--   pen down; move (x2,y2);
+-- }
+line::Cmd
+line = Define "line" ["x1", "y1","x2","y2"] [Pen Up, Move (V "x1")  (V "y1"),Pen Down, Move (V "x2")  (V "y2")]
+
+-- nix(x,y,w,h){
+--   call line (x, y, x+y, y+h);
+--   call line (x+w, y, x, y+h);
+-- }
+
+nix::Cmd
+nix = Define "nix" ["x","y","w","h"] [Call "line" [ V "x",V "y",Add (V "x")(V "w"), Add (V "y")(V "h")],Call "line" [ Add (V "x")(V "w"),V "y",V "x", Add (V "y")(V "h")]]
+
+
+drawStep:: Int-> Int-> Prog
+drawStep x y = [Call "line" [ V (show x),V (show y), V (show (x-1)), V (show y)], Call "line" [ V (show (x-1)),V (show y), V (show (x-1)), V (show (y-1))]] 
+
+macToStr:: Macro -> String
+macToStr m = (show m)
+
+steps:: Int -> Prog 
+steps 0 = [Pen Up]
+steps n = drawStep (n) (n) ++ steps (n - 1)
+
+macCounter:: Prog -> [Macro]
+macCounter ((Define m v (y:ys)):xs) = [m] ++ macCounter (y:ys)
+macCounter (x:xs) = macCounter (xs)
+macCounter [] = []
+
+
+macros:: Prog -> [Macro]
+macros (x:xs) = macCounter [(x)] ++ macCounter (xs)
+
+printVars:: [Expr] -> String
+printVars (x:xs) = printExp (x) ++ "," ++ printVars (xs)
+printVars [x] = printExp(x)
+printVars [] = []
+
+
+printVar:: [Var] -> String
+printVar (x:xs) = x ++ "," ++ printVar (xs)
+printVar [x] = x
+printVar [] = []
+
+printExp:: Expr -> String
+printExp (V x) = x
+printExp (N x) = show x
+printExp (Add x y) = (printExp x) ++ "+" ++ (printExp y)
+
+
+printMode:: Mode -> String
+printMode Up = "up"
+printMode Down = "down"
+
+prettyPrint:: Prog -> String
+prettyPrint ((Define m v (y:ys)):xs) = "define " ++ m ++ " " ++ "(" ++  printVar(v) ++ ") " ++ "{ \n" ++ prettyPrint(y:ys) ++ "}\n" ++ prettyPrint(xs)
+prettyPrint ((Move x y):xs) = "move (" ++ (printExp (x)) ++ "," ++ (printExp (y)) ++ ");\n" ++ prettyPrint(xs)
+prettyPrint ((Pen x):xs) = "pen " ++ (printMode (x)) ++ ";\n" ++ prettyPrint(xs)
+prettyPrint ((Call m e):xs) = "call " ++ m ++ "(" ++ (printVars (e)) ++ ");\n" ++ prettyPrint(xs)
+prettyPrint [] = []
+
